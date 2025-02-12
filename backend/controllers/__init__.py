289d546ccd3a73a2fc916ea014272bc9ad98283b
Thasniem <thfjd7865@gmail.com>
2025-02-12 20:22:07 +0530

    updated

--- backend/controllers/__init__.py
+++ backend/controllers/__init__.py
@@ -1,4 +1,4 @@
 # controllers/__init__.py
-from backend.userController import create_user, get_user, update_user
-from backend.transactionController import create_transaction, get_transactions, update_transaction
-from backend.predictionController import create_prediction,get_prediction,make_prediction
+from backend.controllers.userController import create_user, get_user, update_user
+from backend.controllers.transactionController import create_transaction, get_transactions, update_transaction
+from backend.controllers.predictionController import create_prediction,get_prediction,make_prediction
